3f077e02496dde57e84660dcad697896ccf9fbc5
Franco Guazzetti <francoguazzetti@gmail.com>
2025-04-09 21:36:27 -0300

    Update respuestas_negocio.sql

--- respuestas_negocio.sql
+++ respuestas_negocio.sql
@@ -2,8 +2,8 @@ SET search_path TO public;
 SELECT table_name, column_name, data_type FROM information_schema.COLUMNS WHERE table_schema = 'public';
 
 
-#Listar los usuarios que cumplan años el día de hoy cuya cantidad de ventas
-realizadas en enero 2020 sea superior a 1500.
+--Listar los usuarios que cumplan años el día de hoy cuya cantidad de ventas
+--realizadas en enero 2020 sea superior a 1500.
 
 select c.nombre, c.apellido, COUNT(o.ORDER_ID) as total_ventas
 from customer c join seller s on c.user_id = s.user_id join orders o on o.seller_id = s.seller_id 
@@ -11,11 +11,11 @@ where extract(month from o.order_date) = 1 and extract(month from c.fecha_nacimi
 having COUNT (o.order_id) > 1500;  
 
 
-#Por cada mes del 2020, se solicita el top 5 de usuarios que más vendieron($) en la
+/*Por cada mes del 2020, se solicita el top 5 de usuarios que más vendieron($) en la
 categoría Celulares. Se requiere el mes y año de análisis, nombre y apellido del
 vendedor, cantidad de ventas realizadas, cantidad de productos vendidos y el monto
 total transaccionado.
-Se puso ca.categoria_id = 1 porque celulares, en el ejemplo, se puso en el primer lugar
+Se puso ca.categoria_id = 1 porque celulares, en el ejemplo, se puso en el primer lugar*/
 
 WITH top_5 AS (
     SELECT 
@@ -46,8 +46,8 @@ AND t.rn <= 5
 GROUP BY t.mes, año, c.nombre, c.apellido 
 ORDER BY mes, total_recaudado desc;
 
-# Cree la tabla FINALDIA y cree un procedure que permite cargar items automaticamente teniendo en 
-cuenta los valores de la tabla productos
+/* Cree la tabla FINALDIA y cree un procedure que permite cargar items automaticamente teniendo en 
+cuenta los valores de la tabla productos */
 
 create procedure estadofinal ()
 language plpgsql
